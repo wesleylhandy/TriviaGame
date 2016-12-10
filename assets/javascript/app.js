eb9edd66c59bef5d8d2111cc1bcc9897712c8e53
Wesley Handy <wesleylhandy@gmail.com>
2016-12-10 10:08:06 -0500

    end game scenario coded in js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,14 +5,14 @@ let gameStart = false,
 	gameOver = false,
 	correct = 0,
 	wrong = 0,
-	selectedQuestions = [];
+	randomQs = [];
 
 //wait for window to load before allowing clicks
 
 window.onload = function() {
 
 	randomizeQuestions();
-
+	// click function for answers
 	$(".choice").click(function() { 
 		if (gameStart) {
 			gameClock.stop();
@@ -21,10 +21,11 @@ window.onload = function() {
 			quiz.gameOn();
 		}
 	});
-
+	//shows after each question
 	$("#next-button").click(quiz.gameOn);
 
-
+	//only shows after all questions answered
+	$("#replay-button").click(reset);
 }
 
 function reset() {
@@ -34,7 +35,9 @@ function reset() {
 	gameOver = false,
 	correct = 0,
 	wrong = 0,
-	selectedQuestions = [];
+	randomQs = [];
+	randomizeQuestions();
+	quiz.gameOn();
 }
 
 //question bank as an array of objects
@@ -42,96 +45,96 @@ function reset() {
 
 var questionBank = [
 	{
-		question: "'The Office' was set it what northeastern metropolis?",
-		answers: ["Scranton, PA", "Binghampton, NY", "Lancaster, PA", "Pittsburg, PA" ],
-		correctIndex: 0,
+		q: "'The Office' was set it what northeastern metropolis?",
+		a: ["Scranton, PA", "Binghampton, NY", "Lancaster, PA", "Pittsburg, PA" ],
+		index: 0,
 		correctAnimation: '<iframe src="http://giphy.com/embed/slsmTQGDFhAfC?html5=true&hideSocial=true" width="100%" frameborder="0" class="giphy-embed" allowfullscreen=""></iframe>',
 		wrongAnimation: '<iframe src="http://giphy.com/embed/5ouaUj87wIpP2?html5=true&hideSocial=true" width="100%" frameborder="0" class="giphy-embed" allowfullscreen=""></iframe>'
 	},
 
 	{
-		question: "What was the middle initial of Dunder Mifflin's maladroit manager - Michael Scott?",
-		answers: ["K.", "A.", "J.", "S."],
-		correctIndex: 2,
+		q: "What was the middle initial of Dunder Mifflin's maladroit manager - Michael Scott?",
+		a: ["K.", "A.", "J.", "S."],
+		index: 2,
 		correctAnimation: '<iframe src="http://giphy.com/embed/5wWf7GW1AzV6pF3MaVW?html5=true" width="100%" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>',
 		wrongAnimation: '<iframe src="http://giphy.com/embed/5wWf7H89PisM6An8UAU?html5=true&hideSocial=true" width="100%" frameborder="0" class="giphy-embed" allowfullscreen=""></iframe>'		
 	},
 
 	{
-		question: "Which character was part of the show during season 1?",
-		answers: ["Andy Bernard", "David Wallace", "Mose", "Toby Flenderson" ],
-		correctIndex: 3,
+		q: "Which character was part of the show during season 1?",
+		a: ["Andy Bernard", "David Wallace", "Mose", "Toby Flenderson" ],
+		index: 3,
 		correctAnimation: '<iframe src="http://giphy.com/embed/12yZ3KEf43DfLG?html5=true&hideSocial=true" width="100%" frameborder="0" class="giphy-embed" allowfullscreen=""></iframe>',
 		wrongAnimation: '<iframe src="http://giphy.com/embed/DZnXGa85EDmJq?html5=true&hideSocial=true" width="100%" frameborder="0" class="giphy-embed" allowfullscreen=""></iframe>'
 	},
 
 	{
-		question: "Which of the following characters was not a love interest of Michael?",
-		answers: ["Karen Filipelli","Jan Levinson", "Holly Flax", "Donna"],
-		correctIndex: 0,
+		q: "Which of the following characters was not a love interest of Michael?",
+		a: ["Karen Filipelli","Jan Levinson", "Holly Flax", "Donna"],
+		index: 0,
 		correctAnimation: '<iframe src="http://giphy.com/embed/UvOtZlM2mpnZ6?html5=true&hideSocial=true" width="100%" frameborder="0" class="giphy-embed" allowfullscreen=""></iframe>',
 		wrongAnimation: '<iframe src="http://giphy.com/embed/ocU4BrDT5JTEI?html5=true&hideSocial=true" width="100%" frameborder="0" class="giphy-embed" allowfullscreen=""></iframe>'
 	},
 
 	{
-		question: "Pam and Roy dated for how many years before Pam called off their wedding?",
-		answers: ["Seven Years", "Nine Years", "Three Years", "Five Years"],
-		correctIndex: 1,
+		q: "Pam and Roy dated for how many years before Pam called off their wedding?",
+		a: ["Seven Years", "Nine Years", "Three Years", "Five Years"],
+		index: 1,
 		correctAnimation: '<iframe src="http://giphy.com/embed/B5KV4cBwngJ8I?html5=true&hideSocial=true" width="100%" frameborder="0" class="giphy-embed" allowfullscreen=""></iframe>',
 		wrongAnimation: '<iframe src="http://giphy.com/embed/95c8VYQyrf6gg?html5=true&hideSocial=true" width="100%" frameborder="0" class="giphy-embed" allowfullscreen=""></iframe>'		
 	},
 
 	{
-		question: "Which character won the Dundie for 'Hottest in the Office' every year but the last?",
-		answers: ["Kelly Kapoor", "Ryan Howard", "Pam Beesly", "Erin Hannon"],
-		correctIndex: 1,
+		q: "Which character won the Dundie for 'Hottest in the Office' every year but the last?",
+		a: ["Kelly Kapoor", "Ryan Howard", "Pam Beesly", "Erin Hannon"],
+		index: 1,
 		correctAnimation: '<iframe src="http://giphy.com/embed/jo4vznLSCUVB6?html5=true&hideSocial=true" width="100%" frameborder="0" class="giphy-embed" allowfullscreen=""></iframe>',
 		wrongAnimation: '<iframe src="http://giphy.com/embed/jA4T01RxBv77W?html5=true&hideSocial=true" width="100%" frameborder="0" class="giphy-embed" allowfullscreen=""></iframe>'
 	},
 
 	{
-		question: "Michael breaks up with Pamela's mom, Helene, on her birthday because..",
-		answers: ["Michael thinks he has herpes", 
+		q: "Michael breaks up with Pamela's mom, Helene, on her birthday because..",
+		a: ["Michael thinks he has herpes", 
 				"Michael thinks Jan is having his baby", 
 				"Michael realizes Helene is 60 years old", 
 				"Michael is two-timing with his realtor"],
-		correctIndex: 2,
+		index: 2,
 		correctAnimation: '<iframe src="http://giphy.com/embed/Mhj339AjXjpCM?html5=true&hideSocial=true" width="100%" frameborder="0" class="giphy-embed" allowfullscreen=""></iframe>',
 		wrongAnimation: '<iframe src="http://giphy.com/embed/jfRGnN954yU7u?html5=true&hideSocial=true" width="100%" frameborder="0" class="giphy-embed" allowfullscreen=""></iframe>'
 	},
 
 	{
-		question: "Which famous people cameoed on the show to interview for manager after Michael left?",
-		answers: ["Tom Hanks, Ray Romano, Sally Fields, James Spader",
+		q: "Which famous people cameoed on the show to interview for manager after Michael left?",
+		a: ["Tom Hanks, Ray Romano, Sally Fields, James Spader",
 				"Ray Romano, Roseanne Barr, Kathy Bates, Laurence Fishburne",
 				"Will Arnett, Ray Romano, Jim Carrey, Warren Buffett",
 				"Tina Fey, Jim Carrey, Bill Gates, James Spader" ],
-		correctIndex: 2,
+		index: 2,
 		correctAnimation: '<iframe src="http://giphy.com/embed/y5yzypjVc9u3S?html5=true&hideSocial=true" width="100%" frameborder="0" class="giphy-embed" allowfullscreen=""></iframe>',
 		wrongAnimation: '<iframe src="http://giphy.com/embed/wKXqWCButLQT6?html5=true&hideSocial=true" width="100%" frameborder="0" class="giphy-embed" allowfullscreen=""></iframe>'
 	},
 
 	{
-		question: "Which regular on the show wrote the most episodes?",
-		answers: ["Mindy Kaling (Kelly Kapoor)", 
+		q: "Which regular on the show wrote the most episodes?",
+		a: ["Mindy Kaling (Kelly Kapoor)", 
 				"Steve Carell (Michael Scott)", 
 				"B.J. Novak (Ryan Howard)", 
 				"Paul Lieberstein (Toby Flenderson)"],
-		correctIndex: 0,
+		index: 0,
 		correctAnimation: '<iframe src="http://giphy.com/embed/12Ez4WVD11ko4o?html5=true&hideSocial=true" width="100%" frameborder="0" class="giphy-embed" allowfullscreen=""></iframe>',
 		wrongAnimation: '<iframe src="http://giphy.com/embed/Cz1it5S65QGuA?html5=true&hideSocial=true" width="100%" frameborder="0" class="giphy-embed" allowfullscreen=""></iframe>'
 	},
 
 	{
-		question: "Which of the following did not direct at least one episode of 'The Office'?",
-		answers: ["Steve Carell", "J. J. Abrams", "Jon Favreau", "Michael Bay"],
-		correctIndex: 3,
+		q: "Which of the following did not direct at least one episode of 'The Office'?",
+		a: ["Steve Carell", "J. J. Abrams", "Jon Favreau", "Michael Bay"],
+		index: 3,
 		correctAnimation: '<iframe src="http://giphy.com/embed/yoJC2i270b1mQvcDdK?html5=true&hideSocial=true" width="100%" frameborder="0" class="giphy-embed" allowfullscreen=""></iframe>',
 		wrongAnimation: '<iframe src="http://giphy.com/embed/6ZaYjk1qC1Tby?html5=true&hideSocial=true" width="100%"  frameborder="0" class="giphy-embed" allowfullscreen=""></iframe>'
 	}
 ];
 
-//var gameAudio = new Audio ("../sounds/theme.mp3");
+//var gameAudio = new Audio ("assets/sounds/theme.mp3");
 
 //functions needed for game to operate
 
@@ -140,15 +143,15 @@ var questionBank = [
 
 function randomizeQuestions() {
 	//make a copy of questionBank, so that qbank is unaltered
-	var possibleQuestions = questionBank.slice(0);
+	var possibleQs = questionBank.slice(0);
 	for ( let i = 0; i < questionBank.length; i++) {
 		//get a random question
-		var randomIndex = Math.floor(Math.random() * possibleQuestions.length);
-		var question = possibleQuestions[randomIndex];
+		var randomIndex = Math.floor(Math.random() * possibleQs.length);
+		var question = possibleQs[randomIndex];
 		//push to selected
-		selectedQuestions.push(question);
+		randomQs.push(question);
 		//remove from copied questions
-		possibleQuestions.splice(randomIndex, 1);
+		possibleQs.splice(randomIndex, 1);
 	}
 }
 
@@ -208,60 +211,70 @@ var quiz = {
 	questionIndex: 10,
 
 	gameOn : function() {
+		// display endgame scenario if no more questions
 		if (quiz.questionIndex === 0) {
 
+			quiz.gameOver();
+
 		} else {
+			//hide results and end-game and display quiz
 			$("#result-screen").css("display", "none");
 			$("#next-button").css("display", "none");
 			$("#end-screen").css("display", "none");
 			$("#question-screen").css("display", "inherit");
-			gameStart = true;
-			gameClock.reset();
-			gameClock.start();
-			quiz.nextQuestion();
+
+			gameStart = true; //flag .choice click function to stay in quiz
+			gameClock.reset(); //reset clock to 30 sec
+			$("#timer").css("display", "inherit"); //display clock
+			gameClock.start(); //start gameClock
+			quiz.getQuestion(); 
 		}
 	},
-
-	nextQuestion: function() {
+	// display questions, one at a time
+	getQuestion: function() {
 		
 		quiz.questionIndex--;
 		//update display
-		$("#question").text(selectedQuestions[quiz.questionIndex].question);
-		$("#0").text(selectedQuestions[quiz.questionIndex].answers[0]);
-		$("#1").text(selectedQuestions[quiz.questionIndex].answers[1]);
-		$("#2").text(selectedQuestions[quiz.questionIndex].answers[2]);
-		$("#3").text(selectedQuestions[quiz.questionIndex].answers[3]);
+		$("#question").text(randomQs[quiz.questionIndex].question);
+		$("#0").text(randomQs[quiz.questionIndex].a[0]);
+		$("#1").text(randomQs[quiz.questionIndex].a[1]);
+		$("#2").text(randomQs[quiz.questionIndex].a[2]);
+		$("#3").text(randomQs[quiz.questionIndex].a[3]);
 	},
-
+	// display animation based on correct/incorrect answer
 	checkAnswer: function(data) {
-		if (parseInt(data) === selectedQuestions[quiz.questionIndex].correctIndex) {
+
+		if (parseInt(data) === randomQs[quiz.questionIndex].index) {
 			correctAnswer = true;
 			correct++;
-			$("#result-gif").html(selectedQuestions[quiz.questionIndex].correctAnimation);
+			$("#result-gif").html(randomQs[quiz.questionIndex].correctAnimation);
 			$("#result-text").html("Correct! Way to go 'Big Tuna!'");
 		} else {
 			correctAnswer = false;
 			wrong++;
-			$("#result-gif").html(selectedQuestions[quiz.questionIndex].wrongAnimation);
+			$("#result-gif").html(randomQs[quiz.questionIndex].wrongAnimation);
 			$("#result-text").html("Incorrect, Plop! The right answer was:<br>"
-				+ selectedQuestions[quiz.questionIndex].answers
-				[selectedQuestions[quiz.questionIndex].correctIndex]);
+				+ randomQs[quiz.questionIndex].a
+				[randomQs[quiz.questionIndex].index]);
 		}
 		$("#question-screen").css("display", "none");
 		$("#result-screen").css("display", "inherit");
 		setTimeout(function(){ $("#next-button").css("display", "inherit");}, 2000);
+	},
+
+	gameOver: function() {	
+		//hide results and show end-game scenario		
+		$("#result-screen").css("display", "none");
+		$("#next-button").css("display", "none");
+		$("#end-screen").css("display", "inherit");
+
+		$("#correct").text(correct);
+		$("#incorrect").text(wrong);
+		$("#percentage").text(Math.round((correct/10)*100) + "%");
+		setTimeout(function(){ $("#replay-button").css("display", "inherit");}, 2000);
+
+
+
 	}
 
 }
-
-// display questions, one at a time
-
-
-
-// click function for answers
-
-// display animation based on correct/incorrect answer
-
-// click function for next question (possibly use setTimeout)
-
-// display endgame scenario
