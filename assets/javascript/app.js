a263d4e184a0e9aa1bf29ceb37b2964974d7404f
Wesley Handy <wesleylhandy@gmail.com>
2016-12-11 20:27:14 -0500

    added game music, using hosted gifs, not urls, & adjusted html

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,9 +7,33 @@ let gameStart = false,
 	wrong = 0,
 	randomQs = [];
 
+//set up audio
+let vol = 1.0, //initial volume setting
+	interval = 250; //interval used in music.fadeout();
+
+let music = {
+	gameAudio: new Audio ("assets/sounds/theme.mp3"),
+	fadeout: function() {
+				let fade = setInterval(
+				  function() {
+				    // Reduce volume by ~.10 as long as it is above 0
+				    if (vol > 0.05) {
+				      vol -= 0.04999;
+				      music.gameAudio.volume = vol;
+				    }
+				    else {
+				      // Stop the setInterval when 0 is reached
+				      clearInterval(fade);
+				    }
+				  }, interval)
+			}
+}
+
 //wait for window to load before allowing clicks
 
 window.onload = function() {
+
+	music.gameAudio.play();
 
 	randomizeQuestions();
 	// click function for answers
@@ -19,6 +43,7 @@ window.onload = function() {
 			quiz.checkAnswer($(this).data("choice"));
 		} else {
 			quiz.gameOn();
+			music.fadeout();
 		}
 	});
 	//shows after each question
@@ -29,11 +54,12 @@ window.onload = function() {
 }
 
 function reset() {
+	music.fadeout();
 	gameStart = false,
 	correctAnswer = false,
 	timeUp = false,
 	gameOver = false,
-	quiz.qIndex = 10;
+	quiz.qIndex = 10,
 	correct = 0,
 	wrong = 0,
 	randomQs = [];
@@ -49,48 +75,60 @@ var questionBank = [
 		q: "'The Office' was set it what northeastern metropolis?",
 		a: ["Scranton, PA", "Binghampton, NY", "Lancaster, PA", "Pittsburg, PA" ],
 		aIndex: 0,
-		correctAnimation: '<iframe src="http://giphy.com/embed/slsmTQGDFhAfC?html5=true&hideSocial=true" width="100%" frameborder="0" class="giphy-embed" allowfullscreen=""></iframe>',
-		wrongAnimation: '<iframe src="http://giphy.com/embed/5ouaUj87wIpP2?html5=true&hideSocial=true" width="100%" frameborder="0" class="giphy-embed" allowfullscreen=""></iframe>'
+		correctAnimation: "assets/images/dm-holiday-win.gif",
+		wrongAnimation: "assets/images/kevin-stupidity-lose.gif"
+		// correctAnimation: '<iframe src="http://giphy.com/embed/slsmTQGDFhAfC?html5=true&hideSocial=true" width="100%" frameborder="0" class="giphy-embed" allowfullscreen=""></iframe>',
+		// wrongAnimation: '<iframe src="http://giphy.com/embed/5ouaUj87wIpP2?html5=true&hideSocial=true" width="100%" frameborder="0" class="giphy-embed" allowfullscreen=""></iframe>'
 	},
 
 	{
 		q: "What was the middle initial of Dunder Mifflin's maladroit manager - Michael Scott?",
 		a: ["K.", "A.", "J.", "S."],
 		aIndex: 2,
-		correctAnimation: '<iframe src="http://giphy.com/embed/5wWf7GW1AzV6pF3MaVW?html5=true" width="100%" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>',
-		wrongAnimation: '<iframe src="http://giphy.com/embed/5wWf7H89PisM6An8UAU?html5=true&hideSocial=true" width="100%" frameborder="0" class="giphy-embed" allowfullscreen=""></iframe>'		
+		correctAnimation: "assets/images/michael-running-win.gif",
+		wrongAnimation: "assets/images/michael-explain-lose.gif"
+		// correctAnimation: '<iframe src="http://giphy.com/embed/5wWf7GW1AzV6pF3MaVW?html5=true" width="100%" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>',
+		// wrongAnimation: '<iframe src="http://giphy.com/embed/5wWf7H89PisM6An8UAU?html5=true&hideSocial=true" width="100%" frameborder="0" class="giphy-embed" allowfullscreen=""></iframe>'		
 	},
 
 	{
 		q: "Which character was part of the show during season 1?",
 		a: ["Andy Bernard", "David Wallace", "Mose", "Toby Flenderson" ],
 		aIndex: 3,
-		correctAnimation: '<iframe src="http://giphy.com/embed/12yZ3KEf43DfLG?html5=true&hideSocial=true" width="100%" frameborder="0" class="giphy-embed" allowfullscreen=""></iframe>',
-		wrongAnimation: '<iframe src="http://giphy.com/embed/DZnXGa85EDmJq?html5=true&hideSocial=true" width="100%" frameborder="0" class="giphy-embed" allowfullscreen=""></iframe>'
+		correctAnimation: "assets/images/dwight-true-win.gif" ,
+		wrongAnimation: "assets/images/michael-no-lose.gif"
+		// correctAnimation: '<iframe src="http://giphy.com/embed/12yZ3KEf43DfLG?html5=true&hideSocial=true" width="100%" frameborder="0" class="giphy-embed" allowfullscreen=""></iframe>',
+		// wrongAnimation: '<iframe src="http://giphy.com/embed/DZnXGa85EDmJq?html5=true&hideSocial=true" width="100%" frameborder="0" class="giphy-embed" allowfullscreen=""></iframe>'
 	},
 
 	{
 		q: "Which of the following characters was not a love interest of Michael?",
 		a: ["Karen Filipelli","Jan Levinson", "Holly Flax", "Donna"],
 		aIndex: 0,
-		correctAnimation: '<iframe src="http://giphy.com/embed/UvOtZlM2mpnZ6?html5=true&hideSocial=true" width="100%" frameborder="0" class="giphy-embed" allowfullscreen=""></iframe>',
-		wrongAnimation: '<iframe src="http://giphy.com/embed/ocU4BrDT5JTEI?html5=true&hideSocial=true" width="100%" frameborder="0" class="giphy-embed" allowfullscreen=""></iframe>'
+		correctAnimation: "assets/images/mspc-win.gif",
+		wrongAnimation: "assets/images/jan-burn-lose.gif"
+		// correctAnimation: '<iframe src="http://giphy.com/embed/UvOtZlM2mpnZ6?html5=true&hideSocial=true" width="100%" frameborder="0" class="giphy-embed" allowfullscreen=""></iframe>',
+		// wrongAnimation: '<iframe src="http://giphy.com/embed/ocU4BrDT5JTEI?html5=true&hideSocial=true" width="100%" frameborder="0" class="giphy-embed" allowfullscreen=""></iframe>'
 	},
 
 	{
 		q: "Pam and Roy dated for how many years before Pam called off their wedding?",
 		a: ["Seven Years", "Nine Years", "Three Years", "Five Years"],
 		aIndex: 1,
-		correctAnimation: '<iframe src="http://giphy.com/embed/B5KV4cBwngJ8I?html5=true&hideSocial=true" width="100%" frameborder="0" class="giphy-embed" allowfullscreen=""></iframe>',
-		wrongAnimation: '<iframe src="http://giphy.com/embed/95c8VYQyrf6gg?html5=true&hideSocial=true" width="100%" frameborder="0" class="giphy-embed" allowfullscreen=""></iframe>'		
+		correctAnimation: "assets/images/pam-nice-win.gif",
+		wrongAnimation: "assets/images/jim-goingon-lose.gif"
+		// correctAnimation: '<iframe src="http://giphy.com/embed/B5KV4cBwngJ8I?html5=true&hideSocial=true" width="100%" frameborder="0" class="giphy-embed" allowfullscreen=""></iframe>',
+		// wrongAnimation: '<iframe src="http://giphy.com/embed/95c8VYQyrf6gg?html5=true&hideSocial=true" width="100%" frameborder="0" class="giphy-embed" allowfullscreen=""></iframe>'		
 	},
 
 	{
 		q: "Which character won the Dundie for 'Hottest in the Office' every year but the last?",
 		a: ["Kelly Kapoor", "Ryan Howard", "Pam Beesly", "Erin Hannon"],
 		aIndex: 1,
-		correctAnimation: '<iframe src="http://giphy.com/embed/jo4vznLSCUVB6?html5=true&hideSocial=true" width="100%" frameborder="0" class="giphy-embed" allowfullscreen=""></iframe>',
-		wrongAnimation: '<iframe src="http://giphy.com/embed/jA4T01RxBv77W?html5=true&hideSocial=true" width="100%" frameborder="0" class="giphy-embed" allowfullscreen=""></iframe>'
+		correctAnimation: "assets/images/pam-dundies-win.gif",
+		wrongAnimation: "assets/images/ryan-notme-lose.gif"
+		// correctAnimation: '<iframe src="http://giphy.com/embed/jo4vznLSCUVB6?html5=true&hideSocial=true" width="100%" frameborder="0" class="giphy-embed" allowfullscreen=""></iframe>',
+		// wrongAnimation: '<iframe src="http://giphy.com/embed/jA4T01RxBv77W?html5=true&hideSocial=true" width="100%" frameborder="0" class="giphy-embed" allowfullscreen=""></iframe>'
 	},
 
 	{
@@ -100,8 +138,10 @@ var questionBank = [
 				"Michael realizes Helene is 60 years old", 
 				"Michael is two-timing with his realtor"],
 		aIndex: 2,
-		correctAnimation: '<iframe src="http://giphy.com/embed/Mhj339AjXjpCM?html5=true&hideSocial=true" width="100%" frameborder="0" class="giphy-embed" allowfullscreen=""></iframe>',
-		wrongAnimation: '<iframe src="http://giphy.com/embed/jfRGnN954yU7u?html5=true&hideSocial=true" width="100%" frameborder="0" class="giphy-embed" allowfullscreen=""></iframe>'
+		correctAnimation: "assets/images/michael-fine-win.gif",
+		wrongAnimation: "assets/images/michael-inside-lose.gif"
+		// correctAnimation: '<iframe src="http://giphy.com/embed/Mhj339AjXjpCM?html5=true&hideSocial=true" width="100%" frameborder="0" class="giphy-embed" allowfullscreen=""></iframe>',
+		// wrongAnimation: '<iframe src="http://giphy.com/embed/jfRGnN954yU7u?html5=true&hideSocial=true" width="100%" frameborder="0" class="giphy-embed" allowfullscreen=""></iframe>'
 	},
 
 	{
@@ -111,8 +151,10 @@ var questionBank = [
 				"Will Arnett, Ray Romano, Jim Carrey, Warren Buffett",
 				"Tina Fey, Jim Carrey, Bill Gates, James Spader" ],
 		aIndex: 2,
-		correctAnimation: '<iframe src="http://giphy.com/embed/y5yzypjVc9u3S?html5=true&hideSocial=true" width="100%" frameborder="0" class="giphy-embed" allowfullscreen=""></iframe>',
-		wrongAnimation: '<iframe src="http://giphy.com/embed/wKXqWCButLQT6?html5=true&hideSocial=true" width="100%" frameborder="0" class="giphy-embed" allowfullscreen=""></iframe>'
+		correctAnimation: "assets/images/jim-darrell-win.gif",
+		wrongAnimation: "assets/images/kevin-chili-lose.gif"
+		// correctAnimation: '<iframe src="http://giphy.com/embed/y5yzypjVc9u3S?html5=true&hideSocial=true" width="100%" frameborder="0" class="giphy-embed" allowfullscreen=""></iframe>',
+		// wrongAnimation: '<iframe src="http://giphy.com/embed/wKXqWCButLQT6?html5=true&hideSocial=true" width="100%" frameborder="0" class="giphy-embed" allowfullscreen=""></iframe>'
 	},
 
 	{
@@ -122,20 +164,24 @@ var questionBank = [
 				"B.J. Novak (Ryan Howard)", 
 				"Paul Lieberstein (Toby Flenderson)"],
 		aIndex: 0,
-		correctAnimation: '<iframe src="http://giphy.com/embed/12Ez4WVD11ko4o?html5=true&hideSocial=true" width="100%" frameborder="0" class="giphy-embed" allowfullscreen=""></iframe>',
-		wrongAnimation: '<iframe src="http://giphy.com/embed/Cz1it5S65QGuA?html5=true&hideSocial=true" width="100%" frameborder="0" class="giphy-embed" allowfullscreen=""></iframe>'
+		correctAnimation: "assets/images/angela-yes-win.gif",
+		wrongAnimation: "assets/images/andy-hard-lose.gif"
+		// correctAnimation: '<iframe src="http://giphy.com/embed/12Ez4WVD11ko4o?html5=true&hideSocial=true" width="100%" frameborder="0" class="giphy-embed" allowfullscreen=""></iframe>',
+		// wrongAnimation: '<iframe src="http://giphy.com/embed/Cz1it5S65QGuA?html5=true&hideSocial=true" width="100%" frameborder="0" class="giphy-embed" allowfullscreen=""></iframe>'
 	},
 
 	{
 		q: "Which of the following did not direct at least one episode of 'The Office'?",
 		a: ["Steve Carell", "J. J. Abrams", "Jon Favreau", "Michael Bay"],
 		aIndex: 3,
-		correctAnimation: '<iframe src="http://giphy.com/embed/yoJC2i270b1mQvcDdK?html5=true&hideSocial=true" width="100%" frameborder="0" class="giphy-embed" allowfullscreen=""></iframe>',
-		wrongAnimation: '<iframe src="http://giphy.com/embed/6ZaYjk1qC1Tby?html5=true&hideSocial=true" width="100%"  frameborder="0" class="giphy-embed" allowfullscreen=""></iframe>'
+		correctAnimation: "assets/images/michael-right-win.gif",
+		wrongAnimation: "assets/images/dwight-crying-lose.gif"
+		// correctAnimation: '<iframe src="http://giphy.com/embed/yoJC2i270b1mQvcDdK?html5=true&hideSocial=true" width="100%" frameborder="0" class="giphy-embed" allowfullscreen=""></iframe>',
+		// wrongAnimation: '<iframe src="http://giphy.com/embed/6ZaYjk1qC1Tby?html5=true&hideSocial=true" width="100%"  frameborder="0" class="giphy-embed" allowfullscreen=""></iframe>'
 	}
 ];
 
-//var gameAudio = new Audio ("assets/sounds/theme.mp3");
+
 
 //functions needed for game to operate
 
@@ -248,15 +294,22 @@ var quiz = {
 		if (parseInt(data) === randomQs[quiz.qIndex].aIndex) {
 			correctAnswer = true;
 			correct++;
-			$("#result-gif").html(randomQs[quiz.qIndex].correctAnimation);
-			$("#result-text").html("Correct! Way to go 'Big Tuna!'");
+			$("#result-gif").html("<img src='" 
+									+ randomQs[quiz.qIndex].correctAnimation
+									+ "' width = '100%'>");
+
+			$("#result-text").html("Nailed it, Big Tuna!'");
+
 		} else {
 			correctAnswer = false;
 			wrong++;
-			$("#result-gif").html(randomQs[quiz.qIndex].wrongAnimation);
-			$("#result-text").html("Incorrect, Plop! The right answer was:<br>"
-				+ randomQs[quiz.qIndex].a
-				[randomQs[quiz.qIndex].aIndex]);
+			$("#result-gif").html("<img src='"
+									+ randomQs[quiz.qIndex].wrongAnimation
+									+ "' width = '100%'>");
+
+			$("#result-text").html("False!<br>It was: "
+									+ randomQs[quiz.qIndex].a
+									[randomQs[quiz.qIndex].aIndex]);
 		}
 		$("#question-screen").css("display", "none");
 		$("#result-screen").css("display", "inherit");
@@ -272,8 +325,13 @@ var quiz = {
 		$("#correct").text(correct);
 		$("#incorrect").text(wrong);
 		$("#percentage").text(Math.round((correct/10)*100) + "%");
-		setTimeout(function(){ $("#replay-button").css("display", "inherit");}, 2000);
-
+		//play theme music again
+		vol = 1.0;
+		music.gameAudio.volume = 1.0;
+		music.gameAudio.play();
+		//show play again button
+		setTimeout(function(){ $("#replay-button").css("display", "inherit");}, 3000);
+		
 
 
 	}
